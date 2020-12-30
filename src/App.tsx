54a9f979605873699b02030cc36be72ab2ebb814
Sulaiman Panjshiri <sulaiman.panjshiri@excella.com>
2020-12-30 13:57:37 -0500

    add header with styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import NotFound from './pages/notFound/notFound.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
